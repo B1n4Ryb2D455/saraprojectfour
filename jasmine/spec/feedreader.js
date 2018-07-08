712b90c222d8893f235a654f17ec5feed85c0512
Sara Blevins <dvlnthebludrs@gmail.com>
2018-07-08 18:24:57 -0400

    Fix test names to uppercase

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,7 +32,7 @@ $(function () {
         });
     });
 
-    describe('The menu', function () {
+    describe('The Menu', function () {
 
         it('is hidden by default', function () {
             expect($('body').hasClass('menu-hidden')).toBe(true);
@@ -49,7 +49,7 @@ $(function () {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-    describe('Internal entries', function () {
+    describe('Internal Entries', function () {
     /* TODO: Write a test that ensures when the loadFeed
      * function is called and completes its work, there is at least
      * a single .entry element within the .feed container.
@@ -59,7 +59,7 @@ $(function () {
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-    describe('New feed selection', function () {
+    describe('New Feed Selection', function () {
     /* TODO: Write a test that ensures when a new feed is loaded
      * by the loadFeed function that the content actually changes.
      * Remember, loadFeed() is asynchronous.
